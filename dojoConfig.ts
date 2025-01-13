71aa386c39b8d12e344186b163f0c27d0898e98c
root <root@LAPTOP-3SQBB8MJ.>
2025-01-13 21:50:20 +0800

    first commit

---
+++ dojoConfig.ts
@@ -0,0 +1,8 @@
+import { createDojoConfig } from "@dojoengine/core";
+
+import manifest from "../contract/manifest_dev.json";
+
+export const dojoConfig = createDojoConfig({
+    manifest,
+});
+dojoConfig.toriiUrl="http://localhost:8080"
